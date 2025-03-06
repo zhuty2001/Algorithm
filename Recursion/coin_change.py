e98c0407207b0379576a49f7b2064d546ffa8a2b
zhuty2001 <1024989649@qq.com>
2025-03-06 13:33:25 -0600

    udate

--- Recursion/coin_change.py
+++ Recursion/coin_change.py
@@ -1,3 +1,11 @@
+# 硬币找零问题
+# 问题描述：给定一组硬币，每个硬币有一个面值，给定一个找零的数值，找出最少的硬币数目
+# 例如：硬币面值为[1, 5, 10, 25]，找零为60，最少硬币数目为3（25+25+10）
+#
+# 递归思想：
+# 1.找零数值等于硬币面值，返回1
+# 2.找零数值大于硬币面值，找零数值减去硬币面值，递归调用
+
 def coin_change(value_list, change):
     min = 9999999
     #min = change
@@ -10,4 +18,4 @@ def coin_change(value_list, change):
                 min = change_times
     return min
 
-print(coin_change([1, 5, 10, 25], 60)) 
+print(coin_change([1, 5, 10, 25], 58)) 
