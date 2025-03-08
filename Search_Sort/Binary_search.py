149df5d071c1a899d656826fb5deac56a1194251
zhuty2001 <1024989649@qq.com>
2025-03-07 18:56:28 -0600

    recursion binary search

--- Search_Sort/Binary_search.py
+++ Search_Sort/Binary_search.py
@@ -1,3 +1,6 @@
+#O(n) = log(n)
+#2^i = n
+
 def binary_search(list, item):
     first = 0
     last = len(list)-1
@@ -12,4 +15,18 @@ def binary_search(list, item):
             last = mid - 1
     return found
 
+def binart_search_recursion(list,item):
+    if list == []:
+        return False
+    else:
+        mid = (len(list)-1) // 2
+        if item == list[mid]:
+            return True, item
+        if list[mid] < item:
+            return binart_search_recursion(list[mid+1:],item)
+        elif list[mid] > item:
+            return binart_search_recursion(list[:mid],item)
+
 print(binary_search([1,4,8,15,26],8))
+print(binart_search_recursion([1,4,8,15,26,30],4))
+
