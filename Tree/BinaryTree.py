b6755657fd8e53f11b420fb567a35dada8fd31ab
zhuty2001 <1024989649@qq.com>
2025-03-12 01:18:27 -0500

    Binary_Tree

---
+++ Tree/BinaryTree.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self,val = 0):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
